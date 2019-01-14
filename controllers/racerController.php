09f7c8ca363c5283d4654afd97bd5774aece032e
hristotonchev <hristotonchev1991@gmail.com>
2019-01-14 13:48:28 +0200

    minor changes

--- controllers/racerController.php
+++ controllers/racerController.php
@@ -29,7 +29,6 @@ class racerController
             ]);
     }
 
-
     public function getPlusOnePoint($name)
     {
 
@@ -98,58 +97,56 @@ class racerController
                 exit;
             }
 
-
-        if (! isset($accessToken)) {
-            if ($helper->getError()) {
-                header('HTTP/1.0 401 Unauthorized');
-                echo "Error: " . $helper->getError() . "\n";
-                echo "Error Code: " . $helper->getErrorCode() . "\n";
-                echo "Error Reason: " . $helper->getErrorReason() . "\n";
-                echo "Error Description: " . $helper->getErrorDescription() . "\n";
-            } else {
-                header('HTTP/1.0 400 Bad Request');
-                echo 'Bad request';
+            if (! isset($accessToken)) {
+                if ($helper->getError()) {
+                    header('HTTP/1.0 401 Unauthorized');
+                    echo "Error: " . $helper->getError() . "\n";
+                    echo "Error Code: " . $helper->getErrorCode() . "\n";
+                    echo "Error Reason: " . $helper->getErrorReason() . "\n";
+                    echo "Error Description: " . $helper->getErrorDescription() . "\n";
+                } else {
+                    header('HTTP/1.0 400 Bad Request');
+                    echo 'Bad request';
+                }
+                exit;
             }
-            exit;
-        }
 
+            // Logged in
+            echo '<h3>Access Token</h3>';
+            var_dump($accessToken->getValue());
 
-        // Logged in
-        echo '<h3>Access Token</h3>';
-        var_dump($accessToken->getValue());
+            // The OAuth 2.0 client handler helps us manage access tokens
+            $oAuth2Client = $fb->getOAuth2Client();
 
-        // The OAuth 2.0 client handler helps us manage access tokens
-        $oAuth2Client = $fb->getOAuth2Client();
+            // Get the access token metadata from /debug_token
+            $tokenMetadata = $oAuth2Client->debugToken($accessToken);
+            echo '<h3>Metadata</h3>';
+            var_dump($tokenMetadata);
 
-        // Get the access token metadata from /debug_token
-        $tokenMetadata = $oAuth2Client->debugToken($accessToken);
-        echo '<h3>Metadata</h3>';
-        var_dump($tokenMetadata);
-
-        // Validation (these will throw FacebookSDKException's when they fail)
-        $tokenMetadata->validateAppId('606990849742582'); // Replace {app-id} with your app id
-        // If you know the user ID this access token belongs to, you can validate it here
-        //$tokenMetadata->validateUserId('123');
+            // Validation (these will throw FacebookSDKException's when they fail)
+            $tokenMetadata->validateAppId('606990849742582'); // Replace {app-id} with your app id
+            // If you know the user ID this access token belongs to, you can validate it here
+            //$tokenMetadata->validateUserId('123');
             $tokenMetadata->validateExpiration();
 
-        if (! $accessToken->isLongLived()) {
-        // Exchanges a short-lived access token for a long-lived one
-        try {
-            $accessToken = $oAuth2Client->getLongLivedAccessToken($accessToken);
-        } catch (Facebook\Exceptions\FacebookSDKException $e) {
-            echo "<p>Error getting long-lived access token: " . $e->getMessage() . "</p>\n\n";
-            exit;
+            if (! $accessToken->isLongLived()) {
+            // Exchanges a short-lived access token for a long-lived one
+            try {
+                $accessToken = $oAuth2Client->getLongLivedAccessToken($accessToken);
+            } catch (Facebook\Exceptions\FacebookSDKException $e) {
+                echo "<p>Error getting long-lived access token: " . $e->getMessage() . "</p>\n\n";
+                exit;
+            }
+
+            echo '<h3>Long-lived</h3>';
+            var_dump($accessToken->getValue());
+            }
+
+            $_SESSION['fb_access_token'] = (string) $accessToken;
+
+            header("Location: http://localhost");
         }
-
-        echo '<h3>Long-lived</h3>';
-        var_dump($accessToken->getValue());
-        }
-
-        $_SESSION['fb_access_token'] = (string) $accessToken;
-
-        header("Location: http://localhost");
     }
-}
 
     /**
     * Function that renders templates and is responsible to collect some data that will be passed on the templates
@@ -158,7 +155,6 @@ class racerController
     {
         require_once($template);
     }
-
 
     /**
     * Function that get user's details from the hash of the url
